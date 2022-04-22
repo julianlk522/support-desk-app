fb2f3ec2dc7b649b8f3b32e473e714922c2cd59f
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-22 09:49:14 -0400

    error handler middleware, install and import express async handler for requests to the mdb database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,24 @@
-const registerUser = () => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = () => {
+//  Register a new user
+//  Route: /api/users
+//  Public Access
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+        res.send('Register Route')
+})
+
+//  Login a user
+//  Route: /api/users/login
+//  Public Access
+const loginUser = asyncHandler(async () => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser, 
