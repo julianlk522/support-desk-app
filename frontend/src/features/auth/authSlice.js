850be19eafe0609ff441943e79f32d109bfd5a6a
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-23 15:23:29 -0400

    installed redux and created authSlice to hold reducer funcs for registering and logging in user

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,28 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+const initialState = {
+    user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+//  Register new user
+export const register = createAsyncThunk('auth/register', async (user, thunk) => {
+    console.log(user)
+})
+
+//  Login user
+export const login = createAsyncThunk('auth/login', async (user, thunk) => {
+    console.log(user)
+})
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {},
+})
+
+export default authSlice.reducer
