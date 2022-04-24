5c5bf6104729db993ea94b1bcaba75b735b7ecfa
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-24 17:53:37 -0400

    private route component blueprint, placed in app.js for routing to /new-ticket only if the user is logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,10 @@ import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import NewTicket from './pages/NewTicket';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}> 
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
