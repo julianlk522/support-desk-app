de0ca125e79e0049e6fac675050483890c5a7472
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-25 17:24:56 -0400

    createTicket func in ticketService, added to reducer file, added ticket selectors and dispatch functions to NewTicket component, BackButton component

---
+++ frontend/src/features/tickets/ticketService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = '/api/tickets/'
+
+//  Create new ticket
+const createTicket = async (ticketData, token) => {
+    
+    // assign bearer token if there is one
+    const config = {
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.post(API_URL, ticketData, config)
+
+    return response.data
+}
+
+const ticketService = {
+    createTicket
+}
+
+export default ticketService
